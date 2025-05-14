203c3c78ec13c2bc24674d37a9bb8bb1e6d62e41
Momena-akhtar <momena.akhtar19@gmail.com>
2025-05-14 23:45:38 +0500

    added frontpage UI changes

--- src/pages/FrontPage/Components/FrontPage/Features.tsx
+++ src/pages/FrontPage/Components/FrontPage/Features.tsx
@@ -1,16 +1,16 @@
 const Features = () => {
     return (
-      <div className="flex gap-4 mt-8">
-  <span className="bg-[#e9d8b5] px-4 py-2 rounded-full cursor-pointer transition duration-300 hover:scale-105 hover:opacity-90">
+      <div className="flex gap-4 font-semibold mt-8">
+  <span className="bg-[#e9d8b5] border border-[#d9c8a5] px-4 py-2 rounded-full cursor-pointer transition duration-300 hover:scale-105 hover:opacity-90">
     Design
   </span>
-  <span className="bg-[#716e63] px-4 py-2 rounded-full cursor-pointer transition duration-300 hover:scale-105 hover:opacity-90">
+  <span className="bg-[#716e63] border border-[#615e53] px-4 py-2 rounded-full cursor-pointer transition duration-300 hover:scale-105 hover:opacity-90">
     Build
   </span>
-  <span className="bg-[#c2623e] px-4 py-2 rounded-full cursor-pointer transition duration-300 hover:scale-105 hover:opacity-90">
+  <span className="bg-[#c2623e] border border-[#b2522e] px-4 py-2 rounded-full cursor-pointer transition duration-300 hover:scale-105 hover:opacity-90">
     Collaborate
   </span>
-  <span className="bg-[#c3c56f] px-4 py-2 rounded-full cursor-pointer transition duration-300 hover:scale-105 hover:opacity-90">
+  <span className="bg-[#c3c56f] border border-[#b3b55f] px-4 py-2 rounded-full cursor-pointer transition duration-300 hover:scale-105 hover:opacity-90">
     Present
   </span>
 </div>
--- src/pages/FrontPage/Components/FrontPage/FrontPage.tsx
+++ src/pages/FrontPage/Components/FrontPage/FrontPage.tsx
@@ -1,23 +1,33 @@
 import Buttons from "./Buttons";
 import Features from "./Features";
-import BackgroundShapes from "./BackgroundVideo";
 import SyncLogo from "../../assets/sync.png"
 
 const FrontPage = () => {
   return (
-<div className="relative flex flex-col justify-center items-start h-screen px-12 bg-gradient-to-t from-[#c39f50] to-[#f5ebd3]"
+<div className="relative flex font-semibold flex-col justify-center bg-gradient-to-t from-[#c4b696] to-[#f0f0f0] items-center h-screen px-12"
 style={{ fontFamily: "'Kumbh Sans', sans-serif" }}>
       
       <img src={SyncLogo} alt="Sync" className="w-70 mb-4 cursor-pointer" />
-      <p className="text-lg text-black max-w-md">
-        Sync helps you design and develop great products, together.
+      <p className="text-lg text-black max-w-md text-center">
+        Sync helps you{' '}
+        <span className="text-2xl font-bold px-2 py-1 bg-gradient-to-r from-red-500/50 to-white rounded-full">
+          design
+        </span>
+        {' '}and{' '}
+        <span className="text-2xl font-bold px-2 py-1 bg-gradient-to-r from-white to-yellow-500/50 rounded-full">
+          develop
+        </span>
+        {' '}great products,{' '}
+        <span className="text-2xl px-2 font-bold py-1 border border-green-500 bg-green-500/20 rounded-full animate-typing transition duration-300 hover:scale-105">
+          together
+        </span>
+        .
       </p>
       <Buttons />
       {/* Position Features at the bottom center */}
       <div className="absolute bottom-10 left-1/2 transform -translate-x-1/2">
         <Features />
       </div>
-      <BackgroundShapes />
     </div>
   );
 };
